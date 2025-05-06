63f9e93aef7d3e229cf932b62b65efb4a81b97f1
24dp1000038-sk <24dp1000038@ds.study.iitm.ac.in>
2025-05-07 00:11:03 +0530

    creating backend and frontend directories

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav>
+        <RouterLink to="/" class="btn btn-outline-success m-2">Home</RouterLink>
+        <RouterLink to="/about" class="btn btn-outline-primary">About</RouterLink>
+      </nav>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the main content area.</p>
+  </main>
+</template>
